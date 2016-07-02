9b1e970fb137091e35ac5dfab8ce282851b98564
alina-nikols <nikol-pm13@mail.ru>
2016-07-02 17:31:07 +0300

    Update spisok.php

--- spisok.php
+++ spisok.php
@@ -12,49 +12,23 @@ echo "Вы вошли на сайт, как <font color=$color>" .$_SESSION['log
 echo "<br><br>";
 $login = $_SESSION['login'];
 
-function display_db_table($zayavka, $connect)
+$result = mysql_query("SELECT name, phone, problem FROM zayavka WHERE login = '$login' ORDER BY id DESC",$connect);
+
+while($vv = mysql_fetch_assoc($result))
 {
-	$query_string = "SELECT name,phone,problem FROM zayavka";
-	$res_id = mysql_query($query_string, $connect);
-	$column_count = mysql_num_fields($res_id);
-	print ("<TABLE BORDER=1>\n");
-	while ($row = mysql_fetch_row($res_id))
-	{
-	
-		for ($column_num=0; $column_num<$column_count; $column_num++)
-			print ("<TD>$row[$column_num]</TD\n");
-		print("</TD>\n");
-	}
-	print ("<TABLE>\n");
-	}
+	echo "".$vv['name']."<br>-------------------------------------------------------<br>";
+}
 
 if($login == "admin")
 {
-	function display_db_table($zayavka, $connect)
-{
-	$query_string = "SELECT * FROM zayavka";
-	$res_id = mysql_query($query_string, $connect);
-	$column_count = mysql_num_fields($res_id);
-	print ("<TABLE BORDER=1>\n");
-	while ($row = mysql_fetch_row($res_id))
+	$result = mysql_query("SELECT * FROM zayavka ORDER BY id DESC",$connect);
+	while($vv = mysql_fetch_assoc($result))
 	{
-	
-		for ($column_num=0; $column_num<$column_count; $column_num++)
-			print ("<TD>$row[$column_num]</TD\n");
-		print("</TD>\n");
-	}
-	print ("<TABLE>\n");
+		echo "Логин: ".$vv['login']."<br>Заявка: ".$vv['name']."<br>-------------------------------------------------------<br>";
 	}
 }
 }
 ?>
-
-<HTML>
-<BODY>
-<TABLE><TR><TD>
-<?php display_db_table("", $connect)?>
-</TD></TR></TABLE></BODY></HTML>
-
 
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <b><br>Просмотр данных о заявке</b>
